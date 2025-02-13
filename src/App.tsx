7bb70d4334ef2e5b8e00689dabca53c79c6ebc0b
Geoffroy Lesage <gefthefrench@gmail.com>
2025-02-14 01:21:37 +1100

    Move loading into component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
@@ -16,10 +17,6 @@ type EdgeRes = {
 	contributions: Tables<"Contributions">[];
 };
 
-const {
-	data: { gifts, contributions },
-} = await axios<EdgeRes>(api);
-
 // If this is a redirect from Stripe
 const urlParams = new URLSearchParams(window.location.search);
 const stripeId = Array.from(urlParams.entries()).find((param) => {
@@ -33,6 +30,26 @@ if (stripeId) {
 }
 
 function App() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [gifts, setGifts] = useState<Tables<"Gifts">[]>([]);
+	const [contributions, setContributions] = useState<Tables<"Contributions">[]>(
+		[]
+	);
+
+	async function fetchGiftsAndContributions() {
+		const data = await axios<EdgeRes>(api);
+		setGifts(data.data.gifts);
+		setContributions(data.data.contributions);
+		setIsLoading(false);
+	}
+
+	useEffect(() => {
+		if (!isLoading && gifts.length < 1) {
+			setIsLoading(true);
+			fetchGiftsAndContributions();
+		}
+	}, [isLoading, gifts]);
+
 	return (
 		<>
 			<Typography variant="h3" padding={4}>
@@ -43,29 +60,34 @@ function App() {
 					Merci pour votre contribution 🎉
 				</Typography>
 			)}
-			<Grid container spacing={4} sx={{ justifyContent: "center" }}>
-				{gifts?.map((gift) => {
-					const progress =
-						contributions?.reduce((acc, curr) => {
-							if (curr.gift_id === gift.id) {
-								return acc + curr.amount;
-							}
-							return acc;
-						}, 0) / gift.price;
-					return (
-						<GiftCard
-							giftId={gift.id}
-							imageUrl={gift.image_url}
-							name={gift.name}
-							description={gift.description}
-							price={gift.price}
-							progress={progress * 100}
-							actionText="Contribute"
-							actionUrl="https://buy.stripe.com/test_5kA3d1bZbdeFfbGdQQ"
-						/>
-					);
-				})}
-			</Grid>
+
+			{isLoading ? (
+				<div>Finding gifts...</div>
+			) : (
+				<Grid container spacing={4} sx={{ justifyContent: "center" }}>
+					{gifts?.map((gift) => {
+						const progress =
+							contributions?.reduce((acc, curr) => {
+								if (curr.gift_id === gift.id) {
+									return acc + curr.amount;
+								}
+								return acc;
+							}, 0) / gift.price;
+						return (
+							<GiftCard
+								giftId={gift.id}
+								imageUrl={gift.image_url}
+								name={gift.name}
+								description={gift.description}
+								price={gift.price}
+								progress={progress * 100}
+								actionText="Contribute"
+								actionUrl="https://buy.stripe.com/test_5kA3d1bZbdeFfbGdQQ"
+							/>
+						);
+					})}
+				</Grid>
+			)}
 		</>
 	);
 }
