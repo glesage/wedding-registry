ba7c7c0931a77dc47d85d2985a5518d321c03709
Geoffroy Lesage <gefthefrench@gmail.com>
2025-02-14 01:34:31 +1100

    Move to using gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
 	plugins: [react()],
 	server: { allowedHosts: ["geoffroylesage.com", "https://glesage.github.io"] },
+	base: "/wedding-registry/",
 });
