42986e0396f27b789014da517ab2b1ae3158edb7
Geoffroy Lesage <gefthefrench@gmail.com>
2025-02-13 23:36:09 +1100

    Handle cors

--- supabase/functions/get-gifts-and-contributions/index.ts
+++ supabase/functions/get-gifts-and-contributions/index.ts
@@ -1,7 +1,20 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
 
-Deno.serve(async () => {
+import { corsHeaders } from "../_shared/cors.ts";
+
+function response(body: string, status: number) {
+	return new Response(body, {
+		headers: { ...corsHeaders, "Content-Type": "application/json" },
+		status,
+	});
+}
+
+Deno.serve(async (req) => {
+	if (req.method === "OPTIONS") {
+		return new Response("ok", { headers: corsHeaders });
+	}
+
 	const client = createClient(
 		Deno.env.get("SUPABASE_URL") ?? "",
 		Deno.env.get("SUPABASE_ANON_KEY") ?? ""
@@ -14,14 +27,8 @@ Deno.serve(async () => {
 		client.from("Gifts").select("*"),
 		client.from("Contributions").select("amount, gift_id"),
 	]);
-	if (giftsError) {
-		return new Response(giftsError.message, { status: 400 });
-	}
-	if (contributionsError) {
-		return new Response(contributionsError.message, { status: 400 });
-	}
+	if (giftsError) return response(giftsError.message, 400);
+	if (contributionsError) return response(contributionsError.message, 400);
 
-	return new Response(JSON.stringify({ gifts, contributions }), {
-		headers: { "Content-Type": "application/json" },
-	});
+	return response(JSON.stringify({ gifts, contributions }), 200);
 });
